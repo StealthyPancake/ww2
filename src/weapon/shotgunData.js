e2319fe7aa743fd4c1a3d40a44b39c0501bbc14a
StealthyPancake <stealthy.pancake.gaming@gmail.com>
2018-02-09 15:26:58 -0800

    Update weapon stats

--- src/weapon/shotgunData.js
+++ src/weapon/shotgunData.js
@@ -1,8 +1,8 @@
 let shotgunData = [
-    { name:'Combat Shotgun', type: 'Shotgun', rof: 58, ammo: 7, extAmmo: 10, damage: [{length: 8, amount: 100}, {length: 7, amount: 55}]},
+    { name:'Combat Shotgun', type: 'Shotgun', rof: 58, ammo: 7, extAmmo: 10, damage: [{length: 12, amount: 100}, {length: 3, amount: 55}]},
     { name:'M30', type: 'Shotgun', rof: 150, ammo: 2, damage: [{length: 10, amount: 100}, {length: 5, amount: 90}]},
     { name:'Toggle Action', type: 'Shotgun', rof: 160, ammo: 6, extAmmo: 9, damage: [{length: 2, amount: 110}, {length: 5, amount: 55}, {length: 8, amount: 49}]},
-    { name:'Sawed-off', type: 'Shotgun', rof: 130, ammo: 2, damage: [{length: 4, amount: 100}, {length: 3, amount: 65}, {length: 8, amount: 55}]},
+    { name:'Sawed-off', type: 'Shotgun', rof: 130, ammo: 2, damage: [{length: 7, amount: 100}, {length: 3, amount: 65}, {length: 8, amount: 55}]},
 ];
 
 export default shotgunData;
