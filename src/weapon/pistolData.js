62695219090932c63a4e49a54fce6820c1162570
StealthyPancake <stealthy.pancake.gaming@gmail.com>
2018-03-10 21:37:01 -0800

    Update weapon data

--- src/weapon/pistolData.js
+++ src/weapon/pistolData.js
@@ -1,7 +1,10 @@
 const pistolData = [
     { name:'P-08', type: 'Pistol', rof: 430, ammo: 8, extAmmo: 12, damage: [{length: 38, amount: 40}, {length: '+', amount: 22}]},
     { name:'M1911', type: 'Pistol', rof: 354, ammo: 7, extAmmo: 10, damage: [{length: 9, amount: 50}, {length: 21, amount: 40}, {length: '+', amount: 30}]},
-    { name:'Machine Pistol', type: 'Pistol', rof: 720, ammo: 10, extAmmo: 15, damage: [{length: 4, amount: 30}, {length: 15, amount: 24}, {length: '+', amount: 19}]}
+    { name:'Machine Pistol', type: 'Pistol', rof: 720, ammo: 10, extAmmo: 15, damage: [{length: 4, amount: 30}, {length: 15, amount: 24}, {length: '+', amount: 19}]},
+    { name:'9mm SAP', type: 'Pistol', rof: 462, ammo: 8, extAmmo: 12, damage: [{length: 38, amount: 40}, {length: '+', amount: 22}]},
+    { name:'Enfield No. 2', type: 'Pistol', rof: 280, ammo: 7, extAmmo: 10, damage: [{length: 15, amount: 50}, {length: 30, amount: 40}, {length: '+', amount: 30}]},
+    { name:'Reichsrevolver', type: 'Pistol', rof: 354, ammo: 6, extAmmo: 6, damage: [{length: 9, amount: 48}, {length: 29, amount: 40}, {length: '+', amount: 22}]},
 ];
 
 export default pistolData;
